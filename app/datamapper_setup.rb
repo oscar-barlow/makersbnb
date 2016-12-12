548ccfa56f53506b37c60e717e4e61e0200718b2
ejatkin <ejatkin@gmail.com>
2016-12-12 15:26:59 +0000

    Moved datamapper_setup

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -4,7 +4,7 @@ require_relative './models/user'
 require_relative './models/listing'
 
 def setup
-  DataMapper.setup(:default, "postgres://localhost/makersbnb_test")
+  DataMapper.setup(:default, "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
--- app/models/datamapper_setup.rb
+++
@@ -1,9 +0,0 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require_relative 'user'
-
-def setup
-  DataMapper.setup(:default, "postgres://makersbnb_#{ENV['RACK_ENV']}")
-  DataMapper.finalize
-  DataMapper.auto_upgrade!
-end
