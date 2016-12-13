8b6ef6de68da4f8db027d233557a69bab4a1549c
ejatkin <ejatkin@gmail.com>
2016-12-13 11:41:26 +0000

    WIP - one failing test, swapping driver navigator roles

---
+++ spec/features/create_listing_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'web_helper'
+
+feature "Create listing" do
+  scenario "Signed up user creates new listing" do
+    sign_up
+    visit '/listings/new'
+    fill_in("name", with: "A Lovely Cottage")
+    fill_in("price", with: 12.5)
+    fill_in("description", with: "Includes jacuzzi")
+    click_button("Add listing")
+    expect(page).to have_content("Your listings")
+    expect(page).to have_content("A Lovely Cottage")
+  end
+end
