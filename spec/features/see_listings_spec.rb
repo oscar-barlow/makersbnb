5545bac4ac111e77dd920f38c8b00e24aaa92f03
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-13 17:45:14 +0000

    split tests, refactor out create listing to helper

--- spec/features/see_listings_spec.rb
+++ spec/features/see_listings_spec.rb
@@ -3,21 +3,23 @@ require 'web_helper'
 
 feature "See all listings" do
 
-  scenario "whether use is logged in or not, & they visit the homepage & see all the listings" do
+  before(:each) do
+    visit '/'
+    click_button("Sign up")
+    sign_up
+    create_listing
+  end
 
-      visit '/'
-      click_button("Sign up")
-      sign_up
-      visit '/listing/new'
-      fill_in("name", with: "A Lovely Cottage")
-      fill_in("price", with: 12.5)
-      fill_in("description", with: "Includes jacuzzi")
-      click_button("Add Listing")
+  scenario "user can view the listings on the home page whether if logged in" do
       visit '/'
       expect(page).to have_content("A Lovely Cottage")
-      click_button("Log Out")
-      visit '/'
-      expect(page).to have_content("A Lovely Cottage")
+
+  end
+
+  scenario "user can view listings on home page if not logged in" do
+    log_out
+    visit '/'
+    expect(page).to have_content("A Lovely Cottage")
 
   end
 
