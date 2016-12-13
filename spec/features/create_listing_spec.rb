6e72d72c4856110d9b6df47cff6dcc3b8eaabfc8
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-13 14:19:54 +0000

    typos

--- spec/features/create_listing_spec.rb
+++ spec/features/create_listing_spec.rb
@@ -4,12 +4,12 @@ require 'web_helper'
 feature "Create listing" do
   scenario "Signed up user creates new listing" do
     sign_up
-    visit '/listings/new'
+    visit '/listing/new'
     fill_in("name", with: "A Lovely Cottage")
     fill_in("price", with: 12.5)
     fill_in("description", with: "Includes jacuzzi")
     click_button("Add Listing")
-    expect(page).to have_content("Your listings")
+    expect(page).to have_content("Your Listings")
     expect(page).to have_content("A Lovely Cottage")
   end
 end
