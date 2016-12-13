33a90f4bb888634374b3b1371e993fb324c9b744
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-13 11:32:13 +0000

    Added sign in page

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -6,16 +6,24 @@ feature "Log in" do
   before(:each) do
     visit '/sign_up'
     sign_up
+    log_out
   end
 
   it("should allow a user to log into MakersBnb") do
-  visit '/log_in'
-  expect(page.status_code).to eq(200)
+    visit '/log_in'
+    expect(page.status_code).to eq(200)
 
-  fill_in("email", with: "ocsar@gmail.com")
-  fill_in("password", with: "test")
-  click_button("Log In")
-  expect(page.current_path).to eq('/listing')
-  expect(page).to have_content("Hello Oscar")
+    log_in
+    expect(page.current_path).to eq('/listing')
+    expect(page).to have_content("Hello Oscar")
+  end
+
+  it("should not allow me to log in if my email does not match my registered email") do
+    visit '/log_in'
+    expect(page.status_code).to eq(200)
+
+    failed_log_in_wrong_username
+    expect(page.current_path).to eq('/log_in')
+    expect(page).to_not have_content("Your details are incorrect")
   end
 end
