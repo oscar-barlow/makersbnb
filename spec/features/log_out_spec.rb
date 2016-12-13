5c654c34ffd65f698f164b0ba6eac1819593f9bf
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-13 16:14:05 +0000

    changed routes to make restful

--- spec/features/log_out_spec.rb
+++ spec/features/log_out_spec.rb
@@ -4,13 +4,13 @@ require 'web_helper'
 feature "Log Out" do
 
   before(:each) do
-    visit '/sign_up'
+    visit '/user/new'
     sign_up
   end
 
   it("should allow a user to log out of MakersBnB") do
     log_out
     expect(page).to have_content("Log In")
-    expect(page.current_path).to eq ('/log_in')
+    expect(page.current_path).to eq ('/session/new')
   end
 end
