37f0de43947c47b6f78de5c4b42c3abff0336973
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-15 12:17:12 +0000

    remove p debugging

--- app/app.rb
+++ app/app.rb
@@ -92,7 +92,6 @@ class MakersBnB < Sinatra::Base
   end
 
   get '/listing/:id' do
-    p current_user
     @listing = Listing.get(params[:id])
     @unavailables = Unavailable.all(listing_id: params[:id])
     erb :'listing/get'
